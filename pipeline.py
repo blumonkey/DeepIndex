8cede06ace5b1b12ec15770b854cb46a70a81572
blumonkey <samuelsbushi@gmail.com>
2018-04-19 19:00:54 -0600

    Pipeline Complete

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,20 @@ import re
 import argparse
 
 
+import numpy as np
+import utils
+from PIL import Image
+import math
+
+from google.cloud import vision
+from google.cloud.vision import types
+import shutil
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
 def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [convert(c) for c in re.split('-([0-9]+)', key)]
@@ -17,25 +31,48 @@ def DeepIndex(filename):
     base = file_code.split('.', 1)[0]
     temp_dir = tempfile.mkdtemp(base + '_files')
 
-    subprocess.call(['pdftoppm', '-jpeg', '-r', '72', filename, temp_dir +'/' + base])
+    subprocess.call(['pdftoppm', '-jpeg', '-r', '72', filename, os.path.join(temp_dir, base) ])
 
     jpg_images = glob(temp_dir+'/'+base+'-*.jpg')
     jpg_images = natural_sort(jpg_images)
 
     # Call the model and retrieve BBxs and Sections
-
-    # convert blah-1.jpg -crop widthxheight+x+y out.jpg
-    # tesseract out.jpg stdout --oem 1  -l eng
+    client = vision.ImageAnnotatorClient()
     AllBbox = []
 
-    for bbox, ctype, pageNum in AllBbox:
-        subprocess.call(['convert', temp_dir+'/'+base+'-'+pageNum+'.jpg', '-crop',
-                         '{}x{}+{}+{}'.format(bbox.width, bbox.height, bbox.xmin, bbox.xmax), 'out.jpg'])
-        subprocess.call(['tesseract', 'out.jpg', 'out',
-                         '--oem', '1', '-l', 'eng'])
-        with open('out.txt', 'r') as myfile:
-            ocr_text = myfile.read().replace('\n', ' ').strip()
-        print(ctype+': ' + ocr_text)
+    for img_path in jpg_images:
+        image = Image.open(img_path)
+        image_np = load_image_into_numpy_array(image)
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+
+        sess, tensor_dict,image_tensor = utils.initialize()
+        indices = utils.findIndex(image_np_expanded, 0.5, sess, tensor_dict,image_tensor)
+        AllBbox.append(indices)
+
+    pageNum = 1
+    imgNum = 1
+    for ctype, bboxes in AllBbox:
+        for o_class, bbox in zip(ctype, bboxes):
+            subprocess.call(['convert', temp_dir+'/'+base+'-'+str(pageNum)+'.jpg', '-crop',
+                             '{:d}x{:d}+{:d}+{:d}'.format(int(math.ceil(bbox[3])), int(math.ceil(bbox[2])),
+                                                          int(math.floor(bbox[1])), int(math.floor(bbox[0]))), os.path.join(temp_dir, 'out-%d.jpg' % imgNum)])
+
+            with open(os.path.join(temp_dir, 'out-%d.jpg' % imgNum), 'rb') as image:
+                content = image.read()
+
+            img = types.Image(content=content)
+            res = client.text_detection(image=img)
+            text = res.text_annotations[0].description
+            if o_class == 1:
+                o_class = 'TITLE'
+            else:
+                o_class = 'HEADING'
+            print('Class: {}, Text: {}'.format(o_class, text))
+
+            imgNum += 1
+        pageNum += 1
+
+    shutil.rmtree(temp_dir)
 
 
 if __name__ == '__main__':
@@ -45,4 +82,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
 
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/blumonkey/Downloads/DeepIndex-0de2be869214.json"
     DeepIndex(args.filename)
